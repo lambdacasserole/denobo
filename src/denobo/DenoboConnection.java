6c31d01c9def37d12cae81a881770df85fa9fd11
AlexMullen <alexmullen1991@gmail.com>
2013-12-06 17:22:52 +0000

    Refactored

--- src/denobo/DenoboConnection.java
+++ src/denobo/DenoboConnection.java
@@ -11,59 +11,123 @@ import java.net.Socket;
  *
  * @author Saul Johnson, Alex Mullen, Lee Oliver
  */
-public class ConnectionHandler implements Runnable {
+public class DenoboConnection implements Runnable {
 
     /**
      * The socket to handle receiving data from.
      */
     private final Socket connection;
+
+    /**
+     * A thread that handles waiting for data to be received from this connection.
+     */
+    private final Thread receiveThread;
     
     /**
-     * The BufferedReader object to use for reading data from this connection.
+     * The observer that we notify whenever something occurs.
+     */
+    private final DenoboConnectionObserver observer;
+    
+    /**
+     * A boolean flag that is used to signal to the receive thread to terminate
+     * and prevent any actions from occurring anymore on this object in this state.
+     */
+    private boolean disconnected;
+    
+    /**
+     * The BufferedReader object to use for efficiently reading any data we have
+     * received from this connection.
      */
     private BufferedReader connectionReader;
     
     /**
-     * The BufferedWriter object to use for sending data through this connection.
-     * This is a more efficient way of sending data as multiple send requests
-     * are buffered then sent in one batch.
+     * The BufferedWriter object to use for sending data through this
+     * connection. This is a more efficient way of sending data as multiple send
+     * requests are buffered then sent in one batch.
      */
     private BufferedWriter connectionWriter;
-    
 
+    
+    
     /**
      * Creates a ConnectionHandler that will handle receiving data from a
      * socket.
      *
      * @param connection The connection to handle receiving data from
      */
-    public ConnectionHandler(Socket connection) {
+    public DenoboConnection(Socket connection, DenoboConnectionObserver observer) {
         this.connection = connection;
+        this.observer = observer;
+        
         try {
             connectionReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             connectionWriter = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+        
+        disconnected = false;
+        receiveThread = new Thread(this);
+        receiveThread.start();
     }
 
     @Override
     public void run() {
         try {
-            String line;
-            while ((line = connectionReader.readLine()) != null) {            
             
-                // parse line
+            while (!disconnected) {
+                final String line = connectionReader.readLine();
+                if (line == null) {
+                    break;
+                }
                 
+                // parse line
+
+                // let the observer deal with it
+                observer.onReceivedMessage(this, line);
+
             }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-        
-                    
+
         // connection has closed
-            
+
+        // cleanup
+        disconnect();
+
         // remove from portal lists
 
+    }
+    
+    public void send() {
+        
+    }
+
+    /**
+     * Disconnects and frees up any resources used by this DenoboConnection.
+     */
+    public void disconnect() {
+        
+        if (disconnected) {
+            return;
+        }
+        
+        disconnected = true;
+
+        try {
+            // close the socket to the connection which will cause an exception
+            // to be thrown in the receiveThread.
+            connection.close();
+            
+            // wait for the receive thread to terminate
+            receiveThread.join();
+
+            
+            connectionReader.close();
+            connectionWriter.close();
+        } catch (IOException | InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
--- src/denobo/NetworkPortal.java
+++ src/denobo/NetworkPortal.java
@@ -3,43 +3,26 @@ package denobo;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 
  * @author Saul Johnson, Alex Mullen, Lee Oliver
  */
-public class NetworkPortal extends Portal {
+public class NetworkPortal extends Portal implements DenoboConnectionObserver {
 
-    private List<Socket> connections; 
+    private List<DenoboConnection> connections; 
     
     private ServerSocket serverSocket;
     private Thread acceptThread;
     
-    public void connect(String hostName, int portNumber) {
-        try {
-            Socket socket = new Socket(hostName, portNumber);
-            // Handshake.
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+    private boolean disconnecting;
     
-    /**
-     * Listens for connections on the server port and adds 
-     */
-    private void listenForConnections() {
-        while(true) {
-            try {
-                connections.add(serverSocket.accept());
-            } catch (IOException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
-    
+        
     public NetworkPortal(String name, int portNumber) {
         super(name);
+        connections = new ArrayList<>();
         try {
             serverSocket = new ServerSocket(portNumber);
             acceptThread = new Thread() {
@@ -54,10 +37,52 @@ public class NetworkPortal extends Portal {
         }
     }
     
-    public void disconnect() {
-        
+    public void connect(String hostName, int portNumber) {
+        try {
+            Socket socket = new Socket(hostName, portNumber);
+            // Handshake.
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
     
+    /**
+     * Listens for connections on the server port and adds 
+     */
+    private void listenForConnections() {
+        disconnecting = false;
+        while(!disconnecting) {
+            try {
+                Socket acceptedSocket = serverSocket.accept();
+                connections.add(new DenoboConnection(acceptedSocket, this));
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        disconnecting = false;
+    }
+
+    public void disconnect() {
+        try {
+            // first prevent anyone else from connecting 
+            disconnecting = true;
+            serverSocket.close();
+            
+            // wait for the connection accepting thread to terminate
+            acceptThread.join();
+            
+            // close any connections we have
+            for (DenoboConnection currentConnection : connections) {
+                currentConnection.disconnect();
+            }
+            
+            // remove all the connections from our collection
+            connections.clear();
+        } catch (IOException | InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
     @Override
     public void handleMessage(String message) {
         // Outgoing.
@@ -67,5 +92,27 @@ public class NetworkPortal extends Portal {
     public boolean hasRouteToAgent(String name) {
         return false;
     }
+
     
+    
+    @Override
+    public void onAuthenticated(DenoboConnection connection) {
+        
+        ////////////////////////////////////////////////////////////////////////
+        // THIS METHOD COULD POTENTIALLY BE EXECUTED BY MULTIPLE THREADS!     //
+        ////////////////////////////////////////////////////////////////////////
+        
+        
+    }
+
+    @Override
+    public void onReceivedMessage(DenoboConnection connection, String message) {
+                
+        ////////////////////////////////////////////////////////////////////////
+        // THIS METHOD COULD POTENTIALLY BE EXECUTED BY MULTIPLE THREADS!     //
+        ////////////////////////////////////////////////////////////////////////
+        
+        // Received a message (just printing the output for now)
+        System.out.println(message);
+    }
 }
