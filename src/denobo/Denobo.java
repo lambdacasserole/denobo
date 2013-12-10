e717909eabe2474352237b0952b2cb9f67a968ad
AlexMullen <alexmullen1991@gmail.com>
2013-12-10 00:01:37 +0000

    Broken test code
    Broken test code

--- src/denobo/Denobo.java
+++ src/denobo/Denobo.java
@@ -1,11 +1,5 @@
 package denobo;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JFrame;
-
 /**
  *
  * @author Saul
@@ -16,17 +10,61 @@ public class Denobo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    
-        Agent a2 = new Agent("agent2", false);
-        NetworkPortal np2 = new NetworkPortal("networkportal2", 4757);
-        np2.addAgent(a2);
 
-        TestFrame1 t1 = new TestFrame1(a2);
+        Portal p1 = new Portal("portal1");
+        Portal p2 = new Portal("portal2");
+        Portal p3 = new Portal("portal3");
+
+        p1.registerParentPortal(p2);
+        p1.registerParentPortal(p3);
+
+        p2.registerParentPortal(p1);
+        p2.registerParentPortal(p3);
+
+        p3.registerParentPortal(p1);
+        p3.registerParentPortal(p2);
+
         
-        t1.setSize(250, 250);
-        t1.setLocation(100, 450);
-        t1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Agent agent1 = new Agent("agent1", false);
+        agent1.addMessageHandler(new MessageHandler() {
+            @Override
+            public void messageRecieved(Message message) {
+                System.out.println("received message from " + message.getFrom() + ": " + message.getMessage());
+            }
+        });
         
-        t1.setVisible(true);
+        Agent agent2 = new Agent("agent2", false);
+        agent2.addMessageHandler(new MessageHandler() {
+            @Override
+            public void messageRecieved(Message message) {
+                System.out.println("received message from " + message.getFrom() + ": " + message.getMessage());
+            }
+        });
+                
+        Agent agent3 = new Agent("agent3", false);
+        agent2.addMessageHandler(new MessageHandler() {
+            @Override
+            public void messageRecieved(Message message) {
+                System.out.println("received message from " + message.getFrom() + ": " + message.getMessage());
+            }
+        });
+        
+        p1.addAgent(agent1);
+        p2.addAgent(agent2);
+        p3.addAgent(agent3);
+        
+        agent3.sendMessage("agent1", "Testing");
+
+//        Agent a2 = new Agent("agent2", false);
+//        NetworkPortal np2 = new NetworkPortal("networkportal2", 4757);
+//        np2.addAgent(a2);
+//
+//        TestFrame1 t1 = new TestFrame1(a2);
+//        
+//        t1.setSize(250, 250);
+//        t1.setLocation(100, 450);
+//        t1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        
+//        t1.setVisible(true);
     }
 }
