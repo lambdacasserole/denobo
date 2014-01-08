9ad0c17af2fb1ba78de142d5a3cb65337cdc83d6
Odyssic <odyssic@hotmail.co.uk>
2014-01-08 22:18:37 +0000

    Fixed and updated the GUI of DebugAgentWindow
    You're welcome Alex.
    
    I wanna do more with it though so this class is pretty much my baby

--- src/denobo/centralcommand/designer/AgentDebugWindow.java
+++ src/denobo/centralcommand/designer/AgentDebugWindow.java
@@ -6,54 +6,53 @@ import denobo.MessageHandler;
 import denobo.centralcommand.DenoboWindow;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.GridLayout;
+import java.awt.FlowLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 
 /**
  *
  * @author Lee Oliver
  */
 public class AgentDebugWindow extends DenoboWindow implements ActionListener, MessageHandler {
-    
+
     /**
      * New instances of this agent.
      */
     private final Agent agentModel;
-        
     /**
      * Instances of a send button.
      */
     private final JButton sendButton;
-    
     /**
      * Instances of a clear button.
      */
     private final JButton clearButton;
-    
     /**
      * Instances of a text area for the agent name.
      */
     private final JTextArea agentSendArea;
-    
     /**
      * Instances of a text area for the message.
      */
     private final JTextArea messageSendArea;
-    
     /**
-     * Instances of a table that will display the messages that are sent and received.
+     * Instances of a table that will display the messages that are sent and
+     * received.
      */
     private final JTable messageTable;
-    
     /**
      * Instances of the debug table model.
      */
@@ -64,9 +63,9 @@ public class AgentDebugWindow extends DenoboWindow implements ActionListener, Me
         this.agentModel = agentModel;
 
         agentModel.addMessageHandler(this);
-        
-        
-        
+
+
+
         this.setLayout(new BorderLayout());
         this.setTitle(agentModel.getName() + " Debug Window");
         messageTableModel = new DebugAgentTableModel();
@@ -75,77 +74,87 @@ public class AgentDebugWindow extends DenoboWindow implements ActionListener, Me
 
         JPanel northPanel = new JPanel();
         JPanel centerPanel = new JPanel();
-        JPanel southPanel = new JPanel();
-        
+        JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel firstRowPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel secondRowPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel boxSouthPanel = new JPanel();
+        BoxLayout boxLayout = new BoxLayout(boxSouthPanel, BoxLayout.Y_AXIS);
+        boxSouthPanel.setLayout(boxLayout);
+
         this.setAlwaysOnTop(true);
         this.add(northPanel, BorderLayout.NORTH);
         this.add(centerPanel, BorderLayout.CENTER);
         this.add(southPanel, BorderLayout.SOUTH);
-        
+
 
         final JScrollPane messageTableScrollPane = new JScrollPane(messageTable);
         centerPanel.add(messageTableScrollPane);
-        
-        southPanel.setLayout(new GridLayout(1, 3));
+
+
         agentSendArea = new JTextArea(1, 10);
         agentSendArea.setBorder(BorderFactory.createLineBorder(Color.black));
-        southPanel.add(agentSendArea);
-        
-        messageSendArea = new JTextArea(1, 10);
-        messageSendArea.setBorder(BorderFactory.createLineBorder(Color.black));
-        southPanel.add(messageSendArea);
-        
+        firstRowPanel.add(agentSendArea);
+
         sendButton = new JButton("Send");
         sendButton.addActionListener(this);
-        southPanel.add(sendButton);
-        
+        firstRowPanel.add(sendButton);
+
         clearButton = new JButton("Clear");
         clearButton.addActionListener(this);
-        southPanel.add(clearButton);
-        
+        firstRowPanel.add(clearButton);
+
+        boxSouthPanel.add(firstRowPanel);
+
+        messageSendArea = new JTextArea(3, 40);
+        messageSendArea.setBorder(BorderFactory.createLineBorder(Color.black));
+        secondRowPanel.add(messageSendArea);
+
+        boxSouthPanel.add(secondRowPanel);
+
+        southPanel.add(boxSouthPanel);
+
         this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         this.pack();
-        
+
     }
-    
+
     public void showAt(Point position) {
-        
+
         this.setLocation(position);
         this.setVisible(true);
-        
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
         if (e.getSource() == sendButton) {
-            
-            messageTableModel.addRow(new Object[] {"To: " + agentSendArea.getText(), 
-                "mgs: " + messageSendArea.getText()}
-            );
-//            agentRecieveArea.append("To: " + agentSendArea.getText() + "\n");
-//            messageRecieveArea.append("mgs: " + messageSendArea.getText() + "\n");
+
+            messageTableModel.addRow(new Object[]{"To: " + agentSendArea.getText(),
+                "mgs: " + messageSendArea.getText()});
+
             agentModel.sendMessage(agentSendArea.getText(), messageSendArea.getText());
-            
+
         } else if (e.getSource() == clearButton) {
-            
+
             messageTableModel.setRowCount(0);
-            
+
         }
-        
+
     }
 
     @Override
-    public void messageRecieved(Agent agent, Message message) {
-        
-        messageTableModel.addRow(new Object[] {"From: " + agentSendArea.getText(), 
-            "mgs: " + messageSendArea.getText()});
-        
+    public void messageRecieved(Agent agent, final Message message) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                messageTableModel.addRow(new Object[]{"From: " + message.getFrom(),
+                    "mgs: " + message.getData()});
+            }
+        });
     }
 
     @Override
     public void messageIntercepted(Agent agent, Message message) {
-        
     }
-
 }
